f31bd9c83f4bb546bb19f5c444b4a8e30bd32409
Minhazul Islam <minhazulislam.se@gmail.com>
2017-08-07 14:41:01 +0600

    2nd commit: crystal report using grouping

--- CrystalReportApp/Controllers/ProductController.cs
+++ CrystalReportApp/Controllers/ProductController.cs
@@ -41,5 +41,30 @@ namespace CrystalReportApp.Controllers
             return File(stream, "application/pdf", "ListProducts.pdf");
 
         }
+
+        public ActionResult ExportGrouping()
+        {
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Reports/CrystalReportProductGrouping.rpt")));
+            rd.SetDataSource(mde.Products.Select(p => new
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price.Value,
+                Quantity = p.Quantity.Value,
+                CategoryId=p.Category.Id,
+                CategoryName=p.Category.Name
+            }).ToList());
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+            Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            return File(stream, "application/pdf", "ListProductsGrouping.pdf");
+
+        }
 	}
 }
